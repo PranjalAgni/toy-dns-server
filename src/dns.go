6bcad9300a3770dd84179db13f285e0d737f3029
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-01-06 23:56:16 +0530

    :zap: added comments on the DNS lifecycle

--- src/dns.go
+++ src/dns.go
@@ -6,3 +6,11 @@ func main() {
 	var rootNameServer = "198.41.0.4"
 	fmt.Println("Going to build our toy dns resolver ✨", rootNameServer)
 }
+
+// Steps
+// Send DNS query to root name server
+// Root name server will return the NS address of specific TLD
+// Ask the same DNS query to that NS TLD
+// It will provide the address of Authorative NS
+// Ask the IP(dns query) to that address
+// Yay we got our IP
